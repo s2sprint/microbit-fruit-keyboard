7da2308dfcd9b2d2c1542fe033e1d79b0d484adb
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-10-07 20:30:40 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
-input.onPinPressed(TouchPin.P2, function () {
-    music.playTone(294, music.beat(BeatFraction.Whole))
-})
 basic.forever(function () {
-	
+    if (pins.analogReadPin(AnalogPin.P2) < 10) {
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    } else if (600 < pins.analogReadPin(AnalogPin.P2)) {
+        music.ringTone(349)
+    } else {
+        basic.clearScreen()
+    }
 })
