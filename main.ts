e3c9ce4a465d7613fab3f83328a53244ded353f6
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-10-09 09:19:43 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P2) < 10) {
         music.playTone(262, music.beat(BeatFraction.Whole))
-    } else if (600 < pins.analogReadPin(AnalogPin.P2)) {
+    } else if (600 < pins.analogReadPin(AnalogPin.P1)) {
         music.ringTone(349)
     } else {
         basic.clearScreen()
