6f0dbd060ccf1dbcc9dacaa49f9b4c424c19f75d
s2sprint <69420623+s2sprint@users.noreply.github.com>
2020-10-09 13:14:15 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P2) < 10) {
+    if (pins.analogReadPin(AnalogPin.P2) < 20) {
         music.playTone(262, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Heart)
     } else if (600 < pins.analogReadPin(AnalogPin.P1)) {
-        music.ringTone(349)
+        music.playTone(330, music.beat(BeatFraction.Whole))
+        basic.showIcon(IconNames.Duck)
     } else {
         basic.clearScreen()
     }
